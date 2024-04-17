633ce4dd3541018a11f4ff2655f5668be625cbb9
zulfiyya <zulfiyyar27@gmail.com>
2024-04-17 16:34:53 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,30 +1,35 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
         // Reading symptoms from the file and counting them
 
 public class AnalyticsCounter {
-    
-    public static void main(String[] args) {
-        Map<String, Integer> symptomCounts = new TreeMap<>();
+    private ISymptomReader reader;
+    private ISymptomWriter writer;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
-            String line = reader.readLine();
-
-            while (line != null) {
-                symptomCounts.put(line, symptomCounts.getOrDefault(line, 0) + 1);
-                line = reader.readLine(); // Read next line
-            }
-        } catch (Exception e) {
-            System.err.println("Error reading from file: " + e.getMessage());
-        }
-
-		// Using WriteSymptomDataToFile to write the results to a file
-        ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
-		writer.writeSymptoms(symptomCounts);
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
     }
-} 
+    public void processSymptoms () {
+        List<String> symptoms = reader.getSymptoms();
+        Map<String, Integer> symptomCounts = countSymptoms(symptoms);
+        writer.writeSymptoms(symptomCounts);
+    }
+    private Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> symptomCounts = new TreeMap<>();
+        for (String symptom : symptoms) {
+            symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) +1);
+        }
+        return symptomCounts;
+    }
+
+    public static void main(String[] args) {
+ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+ISymptomWriter writer= new WriteSymptomDataToFile("result.out");
+AnalyticsCounter analyticsCounter = new AnalyticsCounter(reader, writer);
+analyticsCounter.processSymptoms();
+    }
+}

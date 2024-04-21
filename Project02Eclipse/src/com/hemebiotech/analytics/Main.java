c15c93fdaf651deda73f86fe181a55629bdeee7e
zulfiyya <zulfiyyar27@gmail.com>
2024-04-21 17:58:42 +0200

    Remove duplicates and unnecessary files

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -12,7 +12,7 @@ public class Main {
      */
     public static void main(String[] args) {
         ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
-        ISymptomWriter writer = new WriteSymptomDataToFile("results.out");
+        ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
         AnalyticsCounter analyticsCounter = new AnalyticsCounter(reader, writer);
 
         analyticsCounter.processSymptoms();

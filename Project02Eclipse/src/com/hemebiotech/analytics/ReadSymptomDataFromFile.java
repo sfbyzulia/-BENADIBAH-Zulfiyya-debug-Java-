e9668796bc9c523918dec4e1c952d34128b8cec5
zulfiyya <zulfiyyar27@gmail.com>
2024-04-21 17:22:55 +0200

    Refactor ReadSymptomDataFromFile with proper Javadoc and formatting

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -7,28 +7,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Simple brute force implementation
- *
+ * Reads symptom data from a specified file.
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
 	
 	/**
+	 * Constructor for creating an object to read symptom data from a file.
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath A full or partial path to file with symptom strings in it, one per line.
 	 */
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
-	
+
+	/**
+	 * Reads symptoms from a file and returns them as a list.
+	 * 
+	 * @return A list of strings, each representing a symptom read from the file.
+	 */
 	@Override
 	public List<String> getSymptoms() {
 		ArrayList<String> result = new ArrayList<String>();
 		
 		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+			try (BufferedReader reader = new BufferedReader (new FileReader(filepath))) {
 				String line = reader.readLine();
 				
 				while (line != null) {
@@ -37,7 +41,7 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 				}
 				reader.close();
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.err.println("Error reading from file:" +e.getMessage());
 			}
 		}
 		

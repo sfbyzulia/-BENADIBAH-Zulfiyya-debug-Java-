b76638e111a2cd2083f77138b36ac651f4f8a5cf
zulfiyya <zulfiyyar27@gmail.com>
2024-04-21 16:43:35 +0200

    Refactor ISymptomWriter with proper Javadoc and formatting

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -2,6 +2,15 @@ package com.hemebiotech.analytics;
 
 import java.util.Map;
 
+/**
+ * This interface represents a class that can write symptoms with their occurences to an output.
+ */
 public interface ISymptomWriter {
+
+   /**
+    * Writes symptoms and their occurences to an output.
+
+    * @param symptoms a map of symptoms and their occurences.
+    */
    void writeSymptoms(Map<String, Integer> symptoms); 
 }

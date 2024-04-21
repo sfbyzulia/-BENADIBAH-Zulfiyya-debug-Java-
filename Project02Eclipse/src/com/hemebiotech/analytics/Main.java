6e8bec44327bf29c0e52c8a902f7756e1777feec
zulfiyya <zulfiyyar27@gmail.com>
2024-04-21 17:37:51 +0200

    Finalize project and clean up code

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,6 +1,15 @@
 package com.hemebiotech.analytics;
 
+/**
+ * The main class launches the symptom analytics process.
+ */
 public class Main {
+
+    /**
+     * The main method which serves as th enetry point of the program.
+     * 
+     * @param args The command line arguments (not used).
+     */
     public static void main(String[] args) {
         ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
         ISymptomWriter writer = new WriteSymptomDataToFile("results.out");
